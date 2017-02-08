3f59f4cee57da6dbe71047a83db52fab3a984046
dpellegr <pellegrini.dario@gmail.com>
2017-02-08 15:19:58 +0100

    cleaned particle.h

--- tracker/particle.h
+++ tracker/particle.h
@@ -12,21 +12,4 @@ struct Particle {
   Tfloat d;
 };
 
-/*
-struct Particles {
-  size_t size;
-  Tfloat * x;
-  Tfloat * xp;
-  Tfloat * y;
-  Tfloat * yp;
-  Tfloat * z;
-  Tfloat * d;
-
-  __host__ __device__
-  Particle operator[](size_t i) const {
-    return {x[i], xp[i], y[i], yp[i], z[i], d[i]};
-  }
-};
-*/
-
 #endif
